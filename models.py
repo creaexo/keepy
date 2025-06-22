93c1b9d1931e4e4a814ef49ca5fd1d4a62e3d2f8
creaexo <creaexo@gmail.com>
2025-06-22 19:58:36 +0500

    feat: Добавлена модель предметов.

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 
@@ -11,4 +11,14 @@ class Storage(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     img: Mapped[str] = mapped_column(String(100))
+
+
+class Item(Base):
+    __tablename__ = 'item'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    img: Mapped[str] = mapped_column(String(100))
+    storage: Mapped[int] = mapped_column(ForeignKey('storage.id', ondelete='RESTRICT'))
+    info: Mapped[str] = mapped_column(String(200), nullable=True)
+    """Дополнительные информация о предмете, которая поможет его найти, если не помнишь название."""
 
