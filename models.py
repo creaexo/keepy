29156b87c3ee0fd37e1d9b4d65e1fb2e1b62a638
creaexo <creaexo@gmail.com>
2025-06-22 20:36:24 +0500

    refactor: Для id хранилища задан конкретный тип.

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
+from typing import NewType
+
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+
+StorageId_T = NewType('StorageId_T', int)
+"""Тип идентификаторов хранилищ."""
 
 
 class Base(DeclarativeBase):
@@ -18,7 +23,6 @@ class Item(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     img: Mapped[str] = mapped_column(String(100))
-    storage: Mapped[int] = mapped_column(ForeignKey('storage.id', ondelete='RESTRICT'))
+    storage: Mapped[StorageId_T] = mapped_column(ForeignKey('storage.id', ondelete='RESTRICT'))
     info: Mapped[str] = mapped_column(String(200), nullable=True)
     """Дополнительные информация о предмете, которая поможет его найти, если не помнишь название."""
-
