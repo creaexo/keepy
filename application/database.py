db4f04a031b4ac3de0a5aeaace16d793eb7107eb
creaexo <creaexo@gmail.com>
2025-06-29 19:09:29 +0500

    refactor: В приложении указаны типы.

--- application/database.py
+++ application/database.py
@@ -1,4 +1,5 @@
 import os
+from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
@@ -10,11 +11,11 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
 
 
-async def init_db():
+async def init_db() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
